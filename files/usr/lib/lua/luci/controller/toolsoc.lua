100acfce7267c937c5caadc8835dd17e267b8229
Hnatta <66830227+Hnatta@users.noreply.github.com>
2025-09-03 17:53:26 +0700

    Update toolsoc.lua

--- files/usr/lib/lua/luci/controller/toolsoc.lua
+++ files/usr/lib/lua/luci/controller/toolsoc.lua
@@ -1,26 +1,11 @@
 module("luci.controller.toolsoc", package.seeall)
 
 function index()
-  local fs = require "nixio.fs"
-  if not fs then return end
+  -- Menu Services → OC D/E (YAML)
+  entry({"admin", "services", "yaml"},
+        template("yaml"), _("OC D/E"), 10).leaf = true
 
-  -- OC D/E → yaml.html
-  if fs.access("/www/yaml.html") then
-    entry({"admin","services","oc_yaml"}, call("go_yaml"), _("OC D/E"), 10).leaf = true
-  end
-
-  -- OC Ping → logrotor.html
-  if fs.access("/www/logrotor.html") then
-    entry({"admin","services","oc_ping"}, call("go_ping"), _("OC Ping"), 11).leaf = true
-  end
-end
-
-function go_yaml()
-  local http = require "luci.http"
-  http.redirect("/yaml.html")
-end
-
-function go_ping()
-  local http = require "luci.http"
-  http.redirect("/logrotor.html")
+  -- Menu Services → OC Ping (Logrotor)
+  entry({"admin", "services", "logrotor"},
+        template("logrotor"), _("OC Ping"), 11).leaf = true
 end
